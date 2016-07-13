d513d553d86da85d2f6b4e2eb03bcfc8186ee4c3
Aleksey <alger2008@ya.ru>
2016-07-13 16:09:59 +0400

    Add files via upload

--- func_concat-data.js
+++ func_concat-data.js
@@ -26,29 +26,29 @@ function getCombinedData() {
 	}
 
 	throw new SyntaxError('Недопустимый тип данных'); //Генерируем ошибку
-};
-//Функция для проверяет принадлежность всех элементов массива к одному типу данных
-function getAllType(value) {
-	var itemType = getType(value[0]); //Берем за основу для сравнения первый эл-т массива
+	//Функция для проверяет принадлежность всех элементов массива к одному типу данных
+	function getAllType(value) {
+		var itemType = getType(value[0]); //Берем за основу для сравнения первый эл-т массива
 
-	value.forEach( function(item) { //Перебираем массив и сравниваем тип данных
-		
-		if(itemType != getType(item)) {
-			throw new SyntaxError('Разный тип данных');//Генерируем ошибку
-		}
-		
-	});
-	return itemType;
-};
-//Функция определяет тип данных переданного элемента
-function getType(value) { //Согласно условию задачи нам необходимы: строки, массивы, числа
-	if ( Array.isArray(value) ) {
-		 return 'array';
-	}
-	
-	if(typeof value == 'string' || typeof value == 'number') {
-		return typeof value;
-	}
+		value.forEach( function(item) { //Перебираем массив и сравниваем тип данных
+			
+			if(itemType != getType(item)) {
+				throw new SyntaxError('Разный тип данных');//Генерируем ошибку
+			}
+			
+		});
+		return itemType;
+		//Функция определяет тип данных переданного элемента
+		function getType(value) { //Согласно условию задачи нам необходимы: строки, массивы, числа
+			if ( Array.isArray(value) ) {
+				 return 'array';
+			}
+			
+			if(typeof value == 'string' || typeof value == 'number') {
+				return typeof value;
+			}
+		};
+	};
 };
 //Выводим результат или перехватываем ошибку
 try{
