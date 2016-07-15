d9b3a29a7e1a18d811fa6126dce83aba4637bd6a
Aleksey <alger2008@ya.ru>
2016-07-15 17:57:37 +0400

    Add files via upload

--- func_calendar-generate.js
+++ func_calendar-generate.js
@@ -1,29 +1,83 @@
 /*3.	Календарь. Принимает год, месяц. Рисует таблицу с текущим месяцем.
  Сделать стили для дней другого месяца, сегодняшнего дня.
 */
-function getCalendar(year, month) {
+function createCalendar(year, month) {
 	var nowDate = new Date().getDate(),
 		targetDate = new Date(year, month),
 		lastDatePrevMonth = (new Date(year, month, 0)).getDate(),
-		firstDayTargetDate = targetDate.getDay();
-
-	function createTable() {
-		var tableElem = document.createElement('table');
-
-
-
-
-
-
-
-
-		function createRow(obj) {
-			var row = document.createElement('');
-
-			obj.forEach(function(item) {
-				row.appendChild( createCell(item) );
-			})
-			return row;
+		table = document.createElement("table"),
+		lineWeek = [];
+			
+	for (var i = 1; i <= 7; i++) {
+		if (i < getWeekday(targetDate)) {
+			lineWeek.unshift( createCell(lastDatePrevMonth--, "month_other-day") );
+		} else {
+			lineWeek.push( createCell(targetDate.getDate(), "month_day") );
+			targetDate.setDate(targetDate.getDate() + 1);
 		}
 	}
+
+	table.appendChild( wrapRow(lineWeek) );
+	lineWeek.length = 0;
+
+	while (targetDate.getMonth() == month) {
+		if (targetDate.getDate() == nowDate) {
+			var cell = createCell(targetDate.getDate(), "month_day");
+			cell.classList.add("month_day--active");
+			lineWeek.push(cell);
+		} else {
+			lineWeek.push( createCell(targetDate.getDate(), "month_day") );
+		}
+		
+		if ( getWeekday(targetDate) == 7 ) {
+			table.appendChild( wrapRow(lineWeek) );
+			lineWeek.length = 0;
+		}
+		
+		targetDate.setDate(targetDate.getDate() + 1);
+
+		if (targetDate.getMonth() != month && getWeekday(targetDate) != 1) {
+			
+			for (i = getWeekday(targetDate); i <= 7; i++) {
+				lineWeek.push( createCell(targetDate.getDate(), "month_other-day") );
+				targetDate.setDate(targetDate.getDate() + 1);
+			} 
+
+			table.appendChild( wrapRow(lineWeek) );
+
+			break;
+		}
+	}
+
+	table.classList.add("month");
+
+	return table;
+
+	function wrapRow(arr) {
+		var tr = document.createElement("tr");
+
+		arr.forEach(function(item) {
+			tr.appendChild( item );
+		})
+		return tr;
+	};
+
+	function createCell(item, style) {
+		var td = document.createElement("td");
+		td.classList.add(style);
+		td.innerHTML = item;
+		return td;
+	};
+
+	function getWeekday(date) {
+		var weekday = date.getDay();
+		if (weekday == 0) weekday = 7;
+		return weekday;
+	};
 }
+
+var calendar = document.createElement("div");
+calendar.setAttribute("id", "calendar-container");
+calendar.classList.add("calendar-container");
+calendar.appendChild( createCalendar(2016, 6) );
+document.body.appendChild(calendar);
